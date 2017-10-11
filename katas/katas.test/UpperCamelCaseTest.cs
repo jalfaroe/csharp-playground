4669cc57ae067e5b82d284b7253fd9c181545562
jalfaroe <jalfaroe@gmail.com>
2017-10-11 13:39:50 -0600

    Adding UpperCamelCase Kata

---
+++ katas/katas.test/UpperCamelCaseTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace katas.test
+{
+    [TestClass]
+    public class UpperCamelCaseTest
+    {
+        private UpperCamelCase _upperCamelCase;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _upperCamelCase = new UpperCamelCase();
+        }
+
+        [TestMethod]
+        public void ConvertToUpperCamelCase()
+        {
+            _upperCamelCase.ConvertToUpperCamelCase("How can mirrors be real if our eyes aren't real")
+                .Should().Be("How Can Mirrors Be Real If Our Eyes Aren't Real");
+        }
+    }
+}
---
+++ katas/katas/UpperCamelCase.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System;
+
+namespace katas
+{
+    /// <summary>
+    /// From: "How can mirrors be real if our eyes aren't real"
+    /// To:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+    /// </summary>
+    public class UpperCamelCase
+    {
+        public string ConvertToUpperCamelCase(string phrase)
+        {
+            return String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1)));
+        }
+    }
+}
