14f3e3f7586cd475825ec2cb886a9e107caba9e5
Jairo Alfaro <jalfaroe@gmail.com>
2020-09-25 22:33:52 -0600

    Factorial kata

---
+++ katas/katas.test/FactorialRecursiveTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace katas.test
+{
+    [TestClass]
+    public class FactorialRecursiveTest
+    {
+        private FactorialRecursive _factorial;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _factorial = new FactorialRecursive();
+        }
+
+        [TestMethod]
+        public void GetFactorial()
+        {
+            _factorial.GetFactorial(4).Should().Be(24);
+            _factorial.GetFactorial(8).Should().Be(40320);
+            _factorial.GetFactorial(16).Should().Be(20922789888000);
+        }
+    }
+}
---
+++ katas/katas/FactorialRecursive.cs
@@ -0,0 +1,16 @@
+﻿namespace katas
+{
+    public class FactorialRecursive
+    {
+        /// <summary>
+        ///     The factorial of a positive integer n, is the product of all positive integers less than or equal to n.
+        ///     Examples
+        ///     GetFactorial(0) == 1
+        ///     GetFactorial(1) == 1
+        ///     GetFactorial(2) == 2
+        ///     GetFactorial(3) == 6
+        ///     GetFactorial(4) == 24
+        /// </summary>
+        public long GetFactorial(int n) => (n <= 1) ? 1 : GetFactorial(n - 1) * n;
+    }
+}
