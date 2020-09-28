872c062c4e65bf13229302f1ef50f68af6bbcbd6
Jairo Alfaro <jalfaroe@gmail.com>
2020-09-28 16:26:35 -0600

    isogram kata

---
+++ katas/katas.test/IsogramTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace katas.test
+{
+    [TestClass]
+    public class IsogramTest
+    {
+        private Isogram _isogram;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _isogram = new Isogram();
+        }
+
+        [TestMethod]
+        public void IsIsogram()
+        {
+            _isogram.IsIsogram("Dermatoglyphics").Should().BeTrue();
+            _isogram.IsIsogram("aba").Should().BeFalse();
+            _isogram.IsIsogram("moOse").Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsIsogram2()
+        {
+            _isogram.IsIsogram2("Dermatoglyphics").Should().BeTrue();
+            _isogram.IsIsogram2("aba").Should().BeFalse();
+            _isogram.IsIsogram2("moOse").Should().BeFalse();
+        }
+    }
+}
---
+++ katas/katas/Isogram.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace katas
+{
+    /// <summary>
+    ///     An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that
+    ///     determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore
+    ///     letter case.
+    ///     isIsogram "Dermatoglyphics" == true
+    ///     isIsogram "aba" == false
+    ///     isIsogram "moOse" == false -- ignore letter case
+    /// </summary>
+    public class Isogram
+    {
+        public bool IsIsogram(string str)
+        {
+            return str.ToLower().Distinct().Count() == str.Length;
+        }
+
+        public bool IsIsogram2(string str)
+        {
+            str = str.ToLower();
+            for (var i = 0; i < str.Length; i++)
+            for (var j = i + 1; j < str.Length; j++)
+                if (str[i] == str[j])
+                    return false;
+
+            return true;
+        }
+    }
+}
