4da7ce22048d9e8284bfffd05934f7f71f6967e9
Jairo <jalfaroe@gmail.com>
2021-06-20 20:45:37 -0600

    Reversing string v2

--- katas/katas/StringReverse.cs
+++ katas/katas/StringReverse.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace katas
 {
     public class StringReverse
     {
         public string Reverse(string str) => new string(str.Reverse().ToArray());
+
+        public void Reverse(char[] s)
+        {
+            for (var i = 0; i < s.Length / 2; i++)
+            {
+                var temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = temp;
+            }
+        }
     }
 }
