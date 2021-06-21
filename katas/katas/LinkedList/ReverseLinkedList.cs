12c11cd3670d58137aae46c9902dd3a9ec28d40d
Jairo <jalfaroe@gmail.com>
2021-06-21 09:02:14 -0600

    reverse linked list

---
+++ katas/katas/LinkedList/ReverseLinkedList.cs
@@ -0,0 +1,39 @@
+﻿namespace katas.LinkedList
+{
+    public class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            ListNode prev = null;
+            ListNode next = head.next;
+
+            while (next != null)
+            {
+                head.next = prev;
+                prev = head;
+                head = next;
+                next = head.next;
+            }
+
+            head.next = prev;
+            
+            return head;
+        }
+    }
+
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public ListNode next;
+        public int val;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
