00c6b43238bd3f358dc6bbe9af98f21d3af42056
Jairo <jalfaroe@gmail.com>
2021-06-20 22:19:28 -0600

    Anagram kata

---
+++ katas/katas.test/AnagramTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace katas.test
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        private Anagram _anagram;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _anagram = new Anagram();
+        }
+
+        [TestMethod]
+        public void IsPalindrome()
+        {
+            // Arrange
+            var input = new[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+            IList<IList<string>> expectation = new List<IList<string>>
+            {
+                new List<string> {"bat"},
+                new List<string> {"nat", "tan"},
+                new List<string> {"ate", "eat", "tea"}
+            };
+
+            // Act
+            var groupAnagrams = _anagram.GroupAnagrams(input);
+
+            // Assert
+            groupAnagrams.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
---
+++ katas/katas/Anagram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace katas
+{
+    public class Anagram
+    {
+        // Two strings are anagrams if and only if their sorted strings are equal.
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var groupedAnagrams = new Dictionary<string, IList<string>>();
+
+            for (var i = 0; i < strs.Length; i++)
+            {
+                var str = strs[i];
+                var charArray = str.ToCharArray();
+                Array.Sort(charArray);
+                var key = new string(charArray);
+
+                if (groupedAnagrams.TryGetValue(key, out var group))
+                {
+                    group.Add(str);
+                }
+                else
+                {
+                    groupedAnagrams.Add(key, new List<string> {str});
+                }
+
+            }
+
+            return groupedAnagrams.Select(kvp => kvp.Value).ToList();
+        }
+    }
+}
