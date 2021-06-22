e13af71337f16c51d6a79044c35185d70d2cb79a
Jairo <jalfaroe@gmail.com>
2021-06-22 13:58:16 -0600

    adding queue data structure

---
+++ data-structures/DataStructures/Queue/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Queue
+{
+    // A First In First Out collection
+    public class Queue<T>
+    {
+        private readonly LinkedList<T> list = new LinkedList<T>();
+
+        public int Count
+        {
+            get => list.Count;
+        }
+
+        // Adds an item to the back of the queue
+        public void Enqueue(T item)
+        {
+            list.AddLast(item);
+        }
+
+        // Removes and returns the front item from the queue
+        public T Dequeue()
+        {
+            if (list.Count == 0) throw new InvalidOperationException("The queue is empty.");
+
+            var value = list.First.Value;
+
+            list.RemoveFirst();
+
+            return value;
+        }
+
+        // Returns the front item from the queue without removing it from the queue
+        public T Peek()
+        {
+            if (list.Count == 0) throw new InvalidOperationException("The queue is empty.");
+
+            return list.First.Value;
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+    }
+}
---
+++ data-structures/DataStructuresTest/Queue/QueueTests.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures.Queue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresTest.Queue
+{
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        public void Queue_EnqueueDequeue1to10()
+        {
+            var queue = new Queue<int>();
+
+            for (var i = 0; i < 10; i++) queue.Enqueue(i);
+
+            Assert.AreEqual(10, queue.Count);
+
+            for (var expected = 0; expected < 10; expected++)
+            {
+                Assert.AreEqual(expected, queue.Peek());
+                Assert.AreEqual(expected, queue.Dequeue());
+            }
+
+            Assert.AreEqual(0, queue.Count);
+        }
+    }
+}
