6f6b4fc5c01dcf31bdbece28228930814cffe1ff
Jairo <jalfaroe@gmail.com>
2021-06-22 21:11:43 -0600

    adding Simple Linked List

--- data-structures/DataStructures/LinkedList/SimpleLinkedList.cs
+++ data-structures/DataStructures/LinkedList/SimpleLinkedList.cs
@@ -1,11 +1,87 @@
 ﻿namespace DataStructures.LinkedList
 {
-    public class SimpleLinkedListNode
+    public class SimpleLinkedListNode<T>
     {
+        public SimpleLinkedListNode(T value) => Value = value;
+
+        public SimpleLinkedListNode<T> Next { get; set; }
+
+        public T Value { get; }
     }
 
-
-    public class SimpleLinkedList
+    public class SimpleLinkedList<T>
     {
+        private SimpleLinkedListNode<T> _head;
+        private SimpleLinkedListNode<T> _tail;
+
+        public T Head
+        {
+            get => _head.Value;
+        }
+
+        public T Tail
+        {
+            get => _tail.Value;
+        }
+
+        public int Count { get; private set; }
+
+        public void AddHead(T value)
+        {
+            var newNode = new SimpleLinkedListNode<T>(value);
+
+            if (Count == 0)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                var temp = _head;
+                newNode.Next = temp;
+                _head = newNode;
+            }
+
+            Count++;
+        }
+
+        public void AddTail(T value)
+        {
+            var newNode = new SimpleLinkedListNode<T>(value);
+
+            if (Count == 0)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                var temp = _tail;
+                newNode.Next = temp;
+                _tail = newNode;
+            }
+
+            Count++;
+        }
+
+        public bool Find(T value)
+        {
+            var current = _head;
+            while (current != null)
+            {
+                if (current.Value.Equals(value)) return true;
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
     }
 }
---
+++ data-structures/DataStructuresTest/LinkedList/SimpleLinkedListTest.cs
@@ -0,0 +1,66 @@
+﻿using DataStructures.LinkedList;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresTest.LinkedList
+{
+    [TestClass]
+    public class SimpleLinkedListTest
+    {
+        [TestMethod]
+        public void AddFirstTest()
+        {
+            // Arrange
+            var list = new SimpleLinkedList<int>();
+            list.AddHead(1);
+            list.AddHead(2);
+            list.AddHead(3);
+            list.AddHead(4);
+
+            // Act => Assert
+            list.Head.Should().Be(4);
+            list.Tail.Should().Be(1);
+            list.Count.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void AddTailTest()
+        {
+            // Arrange
+            var list = new SimpleLinkedList<int>();
+            list.AddTail(1);
+            list.AddTail(2);
+            list.AddTail(3);
+            list.AddTail(4);
+
+            // Act => Assert
+            list.Head.Should().Be(1);
+            list.Tail.Should().Be(4);
+            list.Count.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void FindValueTest()
+        {
+            // Arrange
+            var list = CreateList();
+
+            // Act => Assert
+            list.Find(1).Should().BeTrue();
+            list.Find(4).Should().BeTrue();
+            list.Find(5).Should().BeFalse();
+        }
+
+        public SimpleLinkedList<int> CreateList()
+        {
+            var list = new SimpleLinkedList<int>();
+
+            list.AddHead(1);
+            list.AddHead(2);
+            list.AddHead(3);
+            list.AddHead(4);
+
+            return list;
+        }
+    }
+}
